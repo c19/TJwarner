19db4c00343a28b720dba205d4f2411e3d65f4f0
c19 <classone2010@gmail.com>
2013-11-19 23:38:57 +0800

    disble button and notify user when submit

--- static/page.js
+++ static/page.js
@@ -10,11 +10,23 @@ if ( obj.attachEvent ) {
 district = document.getElementsByName('district')[0];
 
 bind(district,'change',
-	function(){
+	function () {
 		current = document.getElementsByName('building')[0];
 		current.removeAttribute('name');
 		current.setAttribute('class','hide');
 		newselect = document.getElementById(district.value);
 		newselect.setAttribute('class','');
 		newselect.setAttribute('name','building');
-	});
+	}
+);
+
+button =  document.getElementById('button');
+bind(button, 'click',
+	function () {
+		this.disabled = true;
+		this.value = "正在查询电费，以及发送测试邮件，应该在10秒内完成。";
+		form = document.getElementsByName('input')[0];
+		form.submit();
+		return true;
+	}	
+);
